9aa4efc56daa45abcdfb4487037a01e701102b22
Brian House <email@brian.house>
2021-10-31 11:15:38 -0700

    upgrades

--- TODO.md
+++ TODO.md
@@ -1,15 +1,13 @@
 ### todo
 
-- need canonical highest-res images for all projects and auto-conversion into WebP format 
+- optimize generation functions
+- need canonical highest-res images for all projects and auto-conversion into WebP format
 
-### features
 
-To be generalizeable beyond me, needs configuration for:
-- site title / name
-- social media sites
-- cv should be fully yamlized
-- homepage
-- if files and folders dont exist, it shouldnt fail
+### design
+- spud is design, the rest is content
+- problem is custom pages
+
 
 
 ### notes
--- __main__.py
+++ __main__.py
@@ -4,131 +4,129 @@ import os, sys, yaml, markdown, shutil, jinja2, re, subprocess, time, datetime, 
 from PIL import Image
 from pprint import pprint
 
-rebuild = False
-images = None
 
-def process_images():
-    try:
-        subprocess.check_call("exiftool -all= images/*", shell=True)      # security / helps opengraph
-    except Exception as e:
-        print(e)
-        exit()
-    try:
-        subprocess.check_call("rm images/*_original &> /dev/null", shell=True)
-    except Exception:
-        pass
-    images = {}
-    filenames = list(os.listdir("images"))
-    filenames.sort()
-    for filename in filenames:
-        if ".jpg" not in filename:
-            continue
-        slug = filename.split(".")[0].replace("@2x", "")
-        tag = slug.split("_")[-1]
-        try:
-            int(tag)
-            slug = slug[:-(len(tag) + 1)]
-        except ValueError:
-            pass
-        if slug not in images:
-            images[slug] = []
-        if "@2x" in filename:
-            images[slug][-1][1] = filename
-        else:
-            images[slug].append([filename, None])
-    # print(json.dumps(images, indent=4))
+try:
+    subprocess.check_call("exiftool -all= images/*", shell=True)      # security / helps opengraph
+except Exception as e:
+    print(e)
+try:
+    subprocess.check_call("rm images/*_original &> /dev/null", shell=True)
+except Exception as e:
+    pass
+filenames = [filename for filename in os.listdir("images") if os.path.splitext(filename)[-1] == ".jpg"]
+
+def get_images(name):
+    images = [filename for filename in filenames if filename[:len(name)] == name and "@2x" not in filename]
+    images.sort()
+    for i, image in enumerate(images):
+        hires = [filename for filename in filenames if os.path.splitext(filename)[0] == os.path.splitext(image)[0] + "@2x"]
+        images[i] = image, hires[0] if len(hires) else None
     return images
 
-def build(structure, root, info=None):
-    if structure is None:
-        return False, None
-    else:
-        update_parent = False
-        pages = structure.keys() if type(structure) is dict else structure
-        sibling_data = {}
-        for page in pages:
-            path = os.path.join(root, slugify(page)) # . has to survive
-            parts = root.split('/')[2:]
-            parts.append(slugify(page.split('.')[0]))
-            content = f"pages/{'/'.join(parts)}.yaml"
-            if type(structure) is dict:
-                template = "spud/templates/%s.html" % slugify(page)
-            else:
-                template = "spud/templates/%s.html" % root.split("/")[-1].split("_")[-1][:-1]
-            html_path = os.path.join(path, "index.html")
-            data = {'page': page}
-            if os.path.isfile(content):
-                with open(content) as f:
-                    data.update(yaml.safe_load(f))
-                def md(data):
-                    if type(data) is dict:
-                        if 'text' in data:
-                            data['text'] = markdown.markdown(data['text'].strip())
-                        else:
-                            for key in data:
-                                md(data[key])
-                    elif type(data) is list:
-                        for item in data:
-                            md(item)
-                md(data)
-            build_page = False if ('build' in data and data['build'] is False) else True
-            if not build_page:
-                path = '/'.join(path.split('/')[:-1])
-            else:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-            data['path'] = path
-            update = False
-            page_images = []
-            if slugify(page) in images:
-                for (image, hires) in images[slugify(page)]:
-                    image_source_path = os.path.join("images", image)
-                    image_destination_path = os.path.join(path, image_source_path.split("/")[-1])
-                    if not os.path.isfile(image_destination_path) or os.path.getmtime(image_source_path) > os.path.getmtime(image_destination_path):
-                        shutil.copy(image_source_path, image_destination_path)
-                        print("\t--> copying %s..." % image)
-                        update = True
-                    if hires is not None:
-                        hires_source_path = os.path.join("images", hires)
-                        hires_destination_path = os.path.join(path, hires_source_path.split("/")[-1])
-                        if not os.path.isfile(hires_destination_path) or os.path.getmtime(hires_source_path) > os.path.getmtime(hires_destination_path):
-                            shutil.copy(hires_source_path, hires_destination_path)
-                            print("\t--> copying %s hires..." % hires)
-                            update = True
-                    width, height = Image.open(image_source_path).size
-                    page_images.append((image, width, height, hires))
-            data.update({'images': page_images})
-            if type(structure) is dict:
-                update, child_data = build(structure[page], path, info)
-                if child_data:
-                    data['child_data'] = child_data
-            if os.path.isfile(template) and os.path.isfile(content) and (not os.path.isfile(html_path) or (os.path.getmtime(template) > os.path.getmtime(html_path)) or (os.path.getmtime(content) > os.path.getmtime(html_path))):
-                # print("\t--> updating content: %s\twith template: %s" % (content, template))
-                update = True
-            elif os.path.isfile(template) and (not os.path.isfile(html_path) or (os.path.getmtime(template) > os.path.getmtime(html_path))):
-                # print("\t--> updating template: %s" % template)
-                update = True
-            if rebuild:
-                update = True
-            print(f"PAGE: {root.split('./')[-1]}/{page}")
-            if update:
-                if build_page:
-                    print(f"\t--> updating")
-                    html = render(template, data, image_structure=images, structure=(structure[page] if type(structure) is dict else None), info=info)
+
+def build():
+
+    # load content
+    with open("structure.yaml") as f:
+        structure = yaml.safe_load(f)
+    structure = {'sections': structure['www'], 'info': load_yaml(os.path.join("pages", "www.yaml"))}
+    sections = structure['sections']
+    for section_name, section in sections.items():
+        section = {'content': load_yaml(os.path.join("pages", f"{section_name}.yaml")), 'children': section, 'images': get_images(section_name)}
+        if section['children'] is not None:
+            for c, child_name in enumerate(section['children']):
+                child = {'page': child_name, 'content': load_yaml(os.path.join("pages", f"{section_name}", f"{child_name}.yaml")), 'images': get_images(child_name)}
+                section['children'][c] = child
+        sections[section_name] = section
+
+    # render files
+    directory = "www"
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    template = os.path.join("spud", "templates", "www.html")
+    if os.path.isfile(template):
+        html = render(template, {}, structure=structure, info=structure['info'])
+        html_path = os.path.join("www", "index.html")
+        with open(html_path, 'w') as f:
+            f.write(html)
+
+    for section_name, section in sections.items():
+
+        # top-level section
+        directory = os.path.join("www", section_name)
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+        if section['content'] is not None:
+            template = os.path.join("spud", "templates", f"{section_name}.html")
+            if os.path.isfile(template):
+                html = render(template, section['content'], structure=structure, info=structure['info'])
+                html_path = os.path.join(directory, "index.html")
+                with open(html_path, 'w') as f:
+                    f.write(html)
+        for image in section['images']:
+            for res in image:
+                if res is not None:
+                    source_path = os.path.join("images", res)
+                    destination_path = os.path.join(directory, res)
+                    shutil.copy(source_path, destination_path)
+
+        # subpages
+        if section['children'] is not None:
+            for child in section['children']:
+                child_name = child['page']
+                print(child_name)
+                directory = os.path.join("www", section_name, child_name)
+                if not os.path.isdir(directory):
+                    os.mkdir(directory)
+                template = os.path.join("spud", "templates", "%s.html" % section_name.strip('s') if section_name[-1] == "s" else child_name + ".html")
+                print("\t", template)
+                if os.path.isfile(template):
+                    html = render(template, child['content'], images=child['images'], structure=structure, info=structure['info'], page=child_name)
+                    html_path = os.path.join(directory, "index.html")
                     with open(html_path, 'w') as f:
                         f.write(html)
-                update_parent = True
-            sibling_data[page] = data
-        return update_parent, sibling_data
+                for image in child['images']:
+                    for res in image:
+                        if res is not None:
+                            source_path = os.path.join("images", res)
+                            destination_path = os.path.join(directory, res)
+                            shutil.copy(source_path, destination_path)
+
+
+# how does that work in detecting if the file has changed?
+#             if os.path.isfile(template) and os.path.isfile(content) and (not os.path.isfile(html_path) or (os.path.getmtime(template) > os.path.getmtime(html_path)) or (os.path.getmtime(content) > os.path.getmtime(html_path))):
+
+
+
+def load_yaml(path):
+    data = None
+    if '.' in os.path.splitext(path)[0]:
+        path = path.split('.')[0] + ".yaml"
+    if os.path.isfile(path):
+        with open(path) as f:
+            data = yaml.safe_load(f)
+            md(data)
+    return data
 
 def render(template_name, data=None, **kwargs):
     template_values = kwargs
-    if type(data) == dict:
+    if data is not None:
         template_values.update(data)
     renderer = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
     renderer.filters.update({'slugify': slugify, 'unslugify': unslugify, 'strip_html': strip_html, 'strip_quotes': strip_quotes, 'strslice': strslice, 'is_list': is_list})
     output = renderer.get_template(template_name).render(template_values)
     return output
+
+def md(data):
+    if type(data) is dict:
+        if 'text' in data:
+            data['text'] = markdown.markdown(data['text'].strip())
+        else:
+            for key in data:
+                md(data[key])
+    elif type(data) is list:
+        for item in data:
+            md(item)
 
 def slugify(value, punctuation=False):
     if not punctuation:
@@ -161,30 +159,25 @@ def strslice(s, length):
 def is_list(value):
     return isinstance(value, list)
 
-def copy_static(root):
+def copy_static():
     for filename in os.listdir(os.path.join("spud", "static")):
         if filename[0] != "." and not os.path.isdir(os.path.join("spud", "static", filename)):
-            shutil.copy(os.path.join("spud", "static", filename), f"{root}/")
+            shutil.copy(os.path.join("spud", "static", filename), "www")
     for filename in os.listdir(os.path.join("spud", "static", "js")):
         if filename[0] != "." and not os.path.isdir(os.path.join("static", filename)):
-            shutil.copy(os.path.join("spud", "static", "js", filename), f"{root}/")
+            shutil.copy(os.path.join("spud", "static", "js", filename), "www")
     for filename in os.listdir("icons"):
         if filename[0] != "." and not os.path.isdir(os.path.join("static", filename)):
-            shutil.copy(os.path.join("icons", filename), f"{root}/")
-    shutil.copy("style.css", f"{root}/")
+            shutil.copy(os.path.join("icons", filename), "www")
+    shutil.copy(os.path.join("spud", "style.css"), "www")
 
 if __name__ == "__main__":
     if not len(sys.argv) > 1:
         print("[generate|rebuild|test]")
         exit()
-    with open("structure.yaml") as f:
-        structure = yaml.safe_load(f)
-    with open("info.yaml") as f:
-        info = yaml.safe_load(f)
-    root = list(structure.keys())[0]
     if sys.argv[1] == "test":
         try:
-            subprocess.check_call(f"python3 -m http.server --directory {root}/ 8000", shell=True)
+            subprocess.check_call(f"python3 -m http.server --directory www/ 8000", shell=True)
         except:
             pass
         exit()
@@ -196,6 +189,5 @@ if __name__ == "__main__":
     else:
         print("[generate|rebuild|test]")
         exit()
-    images = process_images()
-    build(structure, ".", info)
-    copy_static(root)
+    build()
+    copy_static()
